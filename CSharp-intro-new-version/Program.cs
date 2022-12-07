25d612e27c50c0706b4b4c2d2908355695176813
Cavid Bashirov <bashirovcavid93gmail.com>
2022-12-07 12:40:50 +0400

    initial

---
+++ CSharp-intro-new-version/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+//int a = 100;
+//int b = 15;
+
+//int result = a + b;
+
+
+//int sum = 0;
+
+//for (int i = 0; i < 12; i++)
+//{
+//    if(i%2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+
+
+//if(sum == 0)
+//{
+//    Console.WriteLine("Duzgun hesablanmayib");
+
+//}else if (sum == 25)
+//{
+//    Console.WriteLine("Duzgundur");
+//}
+//else
+//{
+//    Console.WriteLine("XXXXXX");
+//}
+
+//int[] numbers = new int[10];
+
+//numbers[0] = 1;
+//numbers[1] = 2;
+//numbers[2] = 3; 
+
+//int[] nums = { 1, 2, 300, 4, 5, 600, 7, 8, 9 };
+
+
+
+
+
+//foreach (int item in nums)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine("------------------------------------");
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//n ve m arasindaki ededlerin cemi
+
+
+//int n = 10;
+//int m = 20;
+
+//int sum = 0;
+
+//for (int i = n; i<m; i++)
+//{
+//    sum += i;
+//}
+
+//Console.WriteLine(sum);
+
+
+//
+
+
+
+
+
+
